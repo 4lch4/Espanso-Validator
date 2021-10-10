6483100f92cae2070c224127e00c6562229d372f
4lch4 <git@4lch4.email>
2021-10-10 13:51:01 -0500

    fix: built for ts

--- dist/lib/IOUtil.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class IOUtil {
-    private basePath;
-    constructor(basePath: string);
-    getPackageNames(): Promise<string[]>;
-    getPackageVersions(name: string): Promise<string[]>;
-}
