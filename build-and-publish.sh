bd2a82d98163018d5ed6a16bfff9eb82216ebc53
4lch4 <git@4lch4.email>
2021-10-10 14:42:59 -0500

    build: pushing latest code

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -6,7 +6,7 @@ echo 'code packaged and built, ready to commit'
 
 git add .
 git commit -m 'build: pushing latest code'
-git tag -a 'v$1' -m 'release $1'
+git tag -a "v$1" -m "release $1"
 git push --follow-tags
 
 # ncc build index.js
