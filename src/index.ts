f78d4352d3b76b6b24c5b5ba326ba8a16e52e905
4lch4 <git@4lch4.email>
2021-10-10 13:23:13 -0500

    feat: start of tsc

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import * as core from '@actions/core'
+import { readdir, stat } from 'fs-extra'
+import { join } from 'path'
+
+const packagesDir = join(process.env.GITHUB_WORKSPACE, 'packages')
+
+const main = async () => {
+  try {
+    core.debug(`attempting with packagesDir = ${packagesDir}`)
+
+    core.setOutput('timeStart', new Date().toTimeString())
+
+    const packageNames = await readdir(packagesDir)
+
+    core.info(`Found ${packageNames.length} packages...`)
+
+    for (let x = 0; x < packageNames.length; x++) {
+      const packageName = packageNames[x]
+      const packageDir = join(packagesDir, packageName)
+      const packageStat = await stat(packageDir)
+
+      core.info(`Found package ${packageName} with size ${packageStat.size}`)
+    }
+
+    // for (const packageName of packageNames) {
+    //   const packagePath = join(packagesDir, packageName)
+    //   core.debug(`attempting with packagePath = ${packagePath}`)
+    //   const packageStat = await stat(packagePath)
+
+    //   core.debug(`Found package ${packageName} with size ${packageStat.size}`)
+    // }
+
+    core.setOutput('fileCount', packageNames.length)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
+  .then(() => core.info('done'))
+  .catch(err => console.error(err))
